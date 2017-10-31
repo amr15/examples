c3a747440f65861b40d5f74676a562238a76a511
amraman <amraman1991@gmail.com>
2017-10-30 17:36:27 -0700

    Update post_processing.R

--- post_processing.R
+++ post_processing.R
@@ -1,64 +1,49 @@
 
-#### This is for Version 1.4 of Seurat (updated code for latest version coming up)
-install.packages("devtools")
-library(devtools)
-install_url("https://github.com/satijalab/seurat/releases/download/v1.4.0/Seurat_1.4.0.8.tgz", binary = TRUE)
+#### This is for Version 2.0 of Seurat
 library(Seurat)
 library(dplyr)
 library(Matrix)
 
 #1st step in processing the umi file, check out the plots generated here to see how your data look
 #function takes in your tsv file, and what you'd like to name your project in quotes ie: "project"
-process_umi<- function (file,name){ #taking min cells and max cells to be 1000 and Scaling UMI
- tissue.data = read.table(file, sep = '\t', header=TRUE, row.names=1)
- tissue = new("seurat", raw.data= tissue.data)
- tissue <- Setup(tissue, min.cells = 10, min.genes = 1000, do.scale = T,do.center=F, project = name)
- #qc/sanity plots
- mito.genes <- grep("^mt-", rownames(tissue@data), value = T)
- percent.mito <- colSums(as.array(expm1(tissue@data[mito.genes, ]))) / colSums(as.array(expm1(tissue@data)))
- tissue <- AddMetaData(tissue, percent.mito, "percent.mito")
- VlnPlot(tissue, c("nGene", "nUMI", "percent.mito"), nCol = 3)
- GenePlot(tissue, "nUMI", "nGene")
- return (tissue)}
-
-## To filter out doublets (give seurat object, and low/high umi estimates)
-find_umi_cutoff <-function (tissue,low,high){
-  expected<- .98* length(tissue@data.info$nGene)
-  for (i in low:high){
-    if (length((SubsetData(tissue, subset.name = "nUMI", accept.high = i))@data.info$nGene) > expected){
-      return(i-1)}else {
-        return ("too low")}
-  }
-}
+process_umi<-function(file,name){
+  tissue.data = read.table(file, sep = '\t', header=TRUE, row.names=1)
+  tissue <- CreateSeuratObject(raw.data = tissue.data,project=name, min.cells=10, min.genes=200, normalization.method = "LogNormalize",do.scale=TRUE)
+  mito.genes <- grep("^mt-", rownames(tissue@data), value = T)
+  percent.mito <- colSums(as.array(expm1(tissue@data[mito.genes, ]))) / colSums(as.array(expm1(tissue@data)))
+  tissue <- AddMetaData(tissue, percent.mito, "percent.mito")
+  VlnPlot(tissue, c("nGene", "nUMI", "percent.mito"), nCol = 3)
+  GenePlot(tissue, "nUMI", "nGene")
+  return (tissue)}
 
 ## to generate TSNE Plots (give seurat object, low/high umi estimates for doublet filtering; Density paramter for dbscan clustering)
-process_umi_step2 <- function (tissue, low,high,density_param){
-  val<-find_umi_cutoff(tissue, low,high)
+process_umi_step2 <- function (tissue, density_param,cutoff){
+  val<-cutoff  #find_umi_cutoff based on plot of nUMI vs nGenes
   tissue <- SubsetData(tissue, subset.name = "nUMI", accept.high = val) 
-  tissue <- MeanVarPlot(tissue ,fxn.x = expMean, fxn.y = logVarDivMean, x.low.cutoff = 0.0125, x.high.cutoff = 3, y.cutoff = 0.5, do.contour = F)
-  tissue <- PCA(tissue,do.print = TRUE, pcs.print = 5, genes.print = 5)
-  tissue=project.pca(tissue,do.print=FALSE)
+  tissue <- FindVariableGenes(tissue ,do.plot = FALSE)
+  tissue <- RunPCA(tissue,do.print = FALSE, pc.genes = tissue@var.genes)
+  tissue=ProjectPCA(tissue,do.print=FALSE)
   tissue <- RunTSNE(tissue, dims.use = 1:11, do.fast = T)
-  tissue=DBClustDimension(tissue,1,2,reduction.use = "tsne",G.use =density_param,set.ident = TRUE)
+  tissue=FindClusters(tissue,k.param=density_param, dims.use = 1:13)
   TSNEPlot(tissue,do.label = TRUE)
   return (tissue)}
 
 ##to create average umi tables with all genes for each clusters 
 make_umi_table<-function(tissue,cid,outfile)
-  {
-    c<-tissue@raw.data[,rownames(subset(tissue@data.info, tissue@data.info$DBclust.ident==cid))]
-    print (length(colnames(c))) # this is the number of cells in the cluster 
-    write.table(data.frame(rowMeans(c, na.rm=FALSE, dims=1)), sep = "\t",outfile) 
-  }
-  
+{
+  c<-tissue@raw.data[,rownames(subset(tissue@meta.data, tissue@meta.data$res.0.8==cid))]
+  print (length(colnames(c))) # this is the number of cells in the cluster 
+  write.table(data.frame(rowMeans(c, na.rm=FALSE, dims=1)), sep = "\t",outfile) 
+}
+
 
 #to identify the marker genes and write umi tables 
 identify_clusters <-function(tissue,ncluster,type) {
-  tissue@data.info$orig.ident<-tissue@data.info$DBclust.ident
+  tissue@meta.data$orig.ident<-tissue@meta.data$res.0.8
   tissue=SetAllIdent(tissue,id = "orig.ident")
-  for (i in (2:ncluster)){
+  for (i in (0:ncluster)){
     marker=FindMarkers(tissue,i)
-    if (length(colnames(tissue@raw.data[,rownames(subset(tissue@data.info, tissue@data.info$DBclust.ident==i))]))>100){
+    if (length(colnames(tissue@raw.data[,rownames(subset(tissue@meta.data, tissue@meta.data$res.0.8==i))]))>100){
       marker<-marker[rownames(as.matrix(rowSums(tissue@raw.data[rownames(marker),])) > .05*length(rownames(marker))),]
     }
     else{
@@ -66,6 +51,8 @@ identify_clusters <-function(tissue,ncluster,type) {
     }
     write.csv(marker,paste0(type,".markergenes",i,'.csv'))
     make_umi_table(tissue,i,paste0(type,"cluster",i,"averageumi.txt"))}}
+
+  
     
 # To Plot Genes of your choice in the clusters and clean up your TSNE plot (input is your seurat object and a list of cluster ids that you want to remove from your plot
 clean_tsne<-function(tissue, badcluster, genelist){
