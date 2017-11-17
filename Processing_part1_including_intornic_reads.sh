e1a6327fa2c10eff4e7a792140e0c64eb50a7a40
amraman <amraman1991@gmail.com>
2017-11-17 14:56:40 -0800

    Update Processing_part1_including_intornic_reads.sh

--- Processing_part1_including_intornic_reads.sh
+++ Processing_part1_including_intornic_reads.sh
@@ -1,6 +1,6 @@
 #!/bin/sh                                                                                                                                                                     
 ####################################################################                                                                                                           
-#####This is form Song Chen's pipeline from Kun Zhang's lab#########                                                                                                           
+#####Adapted form Song Chen's pipeline from Kun Zhang's lab#########                                                                                                           
 ####################################################################                                                                                                           
 
 ##change directory paths as necessary
@@ -14,17 +14,16 @@ picard_dir=/mnt/silencer1/home/amraman/Sebastian/Dropseq_intron/jar
 star_dir=/mnt/silencer1/home/amraman/STAR-2.5.2b/bin/Linux_x86_64/
 
 #change to mouse or human or other species 
-ref_dir=humGenomeIndex_99  
+ref_dir=humGenomeIndex_99/gencode_27  
 
 # this plot tells you about the library quality  (tells you what fraction of read x number of cells contain)
 kneeplot_dir=/mnt/silencer1/home/amraman/Sebastian/Sebastian_dropseq                                                          
 
 ### When you run this file you can enter ./file samplename species exononly cells 
-while getopts ":n:s:e:c:" options; do
+while getopts ":n:s:c:" options; do
     case $options in
         n ) sampleName=$OPTARG;; # what you want to call the samples (the prefix before the fastq.gz file) 
-        s ) species=$OPTARG;;    # m or h (for mouse/human --you can always introduce other options) 
-        e ) exonOnly=$OPTARG;;   # (Either True or False (include reads mapping to introns)) 
+        s ) species=$OPTARG;;    # m or h (for mouse/human --you can always introduce other options)
         c ) cells=$OPTARG;;      # (# of barcodes you wish to use in your final output) 
     esac
 done
@@ -62,8 +61,8 @@ EAD=1 DISCARD_READ=True TAG_NAME=XM NUM_BASES_BELOW_QUALITY=1 | \
 $dropseq_dir/FilterBAM TAG_REJECT=XQ I=/dev/stdin O=/dev/stdout | \
 
 
-#$dropseq_dir/TrimStartingSequence INPUT=/dev/stdin OUTPUT=/dev/stdout OUTPUT_SUMMARY=`pwd`/Reports/$sampleName'.adapter_trimming_report.txt' SEQUENCE='TCGTCGGCAGCGTCAGATGTGTA\
-#TAAGAGACAG' MISMATCHES=0 NUM_BASES=5 | \
+$dropseq_dir/TrimStartingSequence INPUT=/dev/stdin OUTPUT=/dev/stdout OUTPUT_SUMMARY=`pwd`/Reports/$sampleName'.adapter_trimming_report.txt' SEQUENCE='TCGTCGGCAGCGTCAGATGTGTA\
+TAAGAGACAG' MISMATCHES=0 NUM_BASES=5 | \
 
 #trimming the poly A tail (since we are only counting transcripts from the 3' end) 
 $dropseq_dir/PolyATrimmer INPUT=/dev/stdin OUTPUT=/dev/stdout MISMATCHES=0 NUM_BASES=6 | \
@@ -73,42 +72,51 @@ java -Xmx8g -jar $picard_dir/picard.jar SamToFastq INPUT=/dev/stdin FASTQ=`pwd`/
 #########MAPPING INSTRUCTIONS ###########################################
 ### Only change this is you are not using mouse or human genome 
 if [ $species = 'h' ]; then
-        refSTAR=humGenomeIndex_99
+        refSTAR=$ref_dir
         refFasta=GRCh38.primary_assembly.genome.fa
-                refGTF=gencode.v24.primary_assembly.annotation.gtf
+                refGTF=gencode.v27.primary_assembly.annotation.gtf
 else
         refSTAR=../musGenomeIndex
         refFasta=mm10_real.fa
                 refGTF=gencode.vM8.primary_assembly.annotation.gtf
 fi
 
-############The actual mapping                                                                                                                                                                           
+############The actual mapping  ##################                                                                                                                                                                         
 $star_dir/STAR --runThreadN 16 --genomeDir $ref_dir --readFilesIn `pwd`/$sampleName'.unaligned.tagged.fastq' --outFileNamePrefix $sampleName. --outSAMunmapped Within
 
 #merge aligned sam file with cell barcode/UMI tagged bam file, correct barcode synthesis error, and generate digital expression matrix                                        \
 ####Instructions for tagging or not tagging reads mapping to introns                                                                                                                                                                               
-if [ $exonOnly = 'F' ]; then
-        TagCommand=TagReadWithGene
-else
-        TagCommand=TagReadWithGeneExon
-fi
-
 
 ###################
-java -Xmx4g -jar $picard_dir/picard.jar SortSam I=$sampleName'.Aligned.out.sam' O=/dev/stdout SO=queryname TMP_DIR=`pwd`/Tmp |\
-java -Xmx4g -jar $picard_dir/picard.jar MergeBamAlignment REFERENCE_SEQUENCE=$ref_dir/$refFasta UNMAPPED_BAM=$sampleName'.unaligned.tagged.bam' ALIGNED_BAM=/dev/stdin O=/dev/\
-stdout INCLUDE_SECONDARY_ALIGNMENTS=false PAIRED_RUN=false |\
+mkdir -p DGE                                                                                                                                                                                 
+###################                                                                                                                                                                                 
 
-$dropseq_dir/$TagCommand I=/dev/stdin O=$sampleName'.aligned.gene.bam' ANNOTATIONS_FILE=$ref_dir/$refGTF TAG=GE
+## take the mapped reads and map them to exon                                                                                                                                                \                                                                                                                                                                                              
+java -Xmx4g -jar $picard_dir/picard.jar SortSam I=$sampleName'.Aligned.out.sam' O=/dev/stdout SO=queryname TMP_DIR=/mnt/thumper/home/amraman/Tmp |\
+java -Xmx4g -jar $picard_dir/picard.jar MergeBamAlignment REFERENCE_SEQUENCE=$ref_dir/$refFasta UNMAPPED_BAM=$sampleName'.unaligned.tagged.bam' ALIGNED_BAM=/dev/stdin O=/dev/stdout PAIRED_R\
+UN=false |\
+$dropseq_dir/TagReadWithGeneExon I=/dev/stdin O=$sampleName'.aligned.gene.bam' ANNOTATIONS_FILE=$ref_dir/$refGTF TAG=GE
 
-$dropseq_dir/DetectBeadSynthesisErrors I=$sampleName'.aligned.gene.bam' O=$sampleName'.aligned.clean.bam' OUTPUT_STATS=`pwd`/Reports/$sampleName'.synthesis_stats.txt' SUMMARY\
-=`pwd`/Reports/$sampleName'.synthesis_stats_summary.txt' NUM_BARCODES=$cells PRIMER_SEQUENCE=TCGTCGGCAGCGTCAGATGTGTATAAGAGACAG
+## take the unmapped reads and map them to gene                                                                                                                                                                                                 
+java -Xmx4g -jar $picard_dir/picard.jar FastqToSam F1=$sampleName'.Unmapped.out.mate1' O=$sample_name'.unmapped.sam' SAMPLE_NAME=$sample_name
+$star_dir/STAR --runThreadN 16 --genomeDir $ref_dir --readFilesIn `pwd`/$sampleName'.Unmapped.out.mate1' --outFileNamePrefix $sampleName'.map2' --outSAMunmapped Within                      \
+java -Xmx4g -jar $picard_dir/picard.jar SortSam I=$sampleName'.map2.Aligned.out.sam' O=/dev/stdout SO=queryname TMP_DIR=/mnt/thumper/home/amraman/Tmp |\
+java -Xmx4g -jar $picard_dir/picard.jar MergeBamAlignment REFERENCE_SEQUENCE=$ref_dir/$refFasta UNMAPPED_BAM=$sampleName'.unaligned.tagged.bam' ALIGNED_BAM=/dev/stdin O=/dev/stdout PAIRED_R\
+UN=false |\
+$dropseq_dir/TagReadWithGene I=/dev/stdin O=$sampleName'.intronaligned.gene.bam' ANNOTATIONS_FILE=$ref_dir/$refGTF TAG=GE
 
-$dropseq_dir/BAMTagHistogram I=$sampleName'.aligned.clean.bam' O=`pwd`/DGE/$sampleName'.readsByBarcode.txt.gz' TAG=XC
+######### Merge reads that mapped just to exons and those that mapped to introns 
+java -Xmx4g -jar $picard_dir/picard.jar MergeSamFiles I=$sampleName'.intronaligned.gene.bam' I=$sampleName'.aligned.gene.bam' O=$sampleName'.aligned_both.gene.bam'
+$dropseq_dir/DetectBeadSynthesisErrors I=$sampleName'.aligned_both.gene.bam' O=$sampleName'.aligned.clean.bam' OUTPUT_STATS=`pwd`/Reports/$sampleName'.synthesis_stats.txt' SUMMARY=`pwd`/Rep\
+orts/$sampleName'.synthesis_stats_summary.txt' NUM_BARCODES=$cells PRIMER_SEQUENCE=TCGTCGGCAGCGTCAGATGTGTATAAGAGACAG
 
-## merge UMIs together (for the same transcript) if there is only a hamming distance of 1 (one basepair mismatch)
-$dropseq_dir/DigitalExpression I=$sampleName'.aligned.clean.bam' O=`pwd`/DGE/$sampleName'.counts.tsv' SUMMARY=`pwd`/Reports/$sampleName'.count_summary.txt' NUM_CORE_BARCODES=\
-$cells EDIT_DISTANCE=1
+#Make a histogram of barcodes 
+$dropseq_dir/BAMTagHistogram I=$sampleName'.aligned_both.gene.bam' O=`pwd`/DGE/$sampleName'.intron_fixreadsByBarcode.txt.gz' TAG=XC
+
+#calaulate the expression matrix 
+$dropseq_dir/DigitalExpression I=$sampleName'.aligned_both.gene.bam' O=`pwd`/DGE/$sampleName'.inton_fix_counts.tsv' SUMMARY=`pwd`/Reports/$sampleName'.inton_fix.count_summary.txt' NUM_CORE_\
+BARCODES=$cells EDIT_DISTANCE=1
+
 
 ##########################################                                                                                                                                     
 mv $sampleName'.Log.out' Reports/
@@ -124,6 +132,5 @@ mv $sampleName'.unaligned.tagged.bam' Tmp/
 mv $sampleName'.Aligned.out.sam' Tmp/
 
 cd DGE
+## For better estimating how many barcodes (cells)  should be analyzed
 $kneeplot_dir/KneePlot.R $sampleName $((cells*5))
-
-
