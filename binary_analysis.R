b6d9f76263e84b69273b514a63f7b9098c934a6c
amraman <amraman1991@gmail.com>
2019-03-24 13:08:27 -0700

    Update binary_analysis.R

--- binary_analysis.R
+++ binary_analysis.R
@@ -4,13 +4,23 @@ library('Rtsne')
 install.packages('ade4')
 library('ade4')
 
-sparse_analysis<-function(tissue) {
+#' Turns RNA-seq datda into a binary matrix of 1(expressed) 0 (not expressed) 
+#'
+#' Retains genes expressed in at least 10 cells and cells that express at least 200 genes 
+
+binarize_matrix <-function(tissue) {
   tissue@raw.data[tissue@raw.data >=1] <-1
   rows<-rownames(as.matrix(which(rowSums(tissue@raw.data)>=10)))
   cols<-rownames(as.matrix(which(colSums(tissue@raw.data)>=200)))
   tissue@scale.data<-tissue@raw.data[rows,cols]
   return(tissue) 
   }
+
+#' Performs TSNE on a scaled distance matrix created from UMI data
+#' 
+#' Takes in a seurat object (tissue) and returns the suerat object
+#'
+#' n can range from 1 to 10, and corresponds to distance metrics here: https://www.rdocumentation.org/packages/ade4/versions/1.7-6/topics/dist.binary
 
 test_metrics<- function(tissue,n){
   distance<-dist.binary(t(sample@scale.data), method=n, diag = TRUE, upper = FALSE)
