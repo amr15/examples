85a5b459e169793de14d79e07ed07959448f7f4a
amraman <amraman1991@gmail.com>
2019-03-25 11:51:20 -0700

    Update marker_vplots.py

--- marker_vplots.py
+++ marker_vplots.py
@@ -21,34 +21,39 @@ def merged_data(expression, metadata):
     merge[merge.columns[-1]]= merge[merge.columns[-1]].apply(lambda x: float(x))
     return merge 
 
-merged= merged_data(argv[1], argv[2])
+#' This plots the distribution of each marker gene as a violinplot in a subplot of the main figure for 8 genes and 9 cluster ids
+#' merged is the merged data frame where rows are cells, columns are genes, and the last column is the cluster id
+#' cluster_ids is a list of the cluster_ids ie: [0,1,2,3,4,5,6,7,8,9]
+#' figure_name is the name of the pdf you want to save the violinplots as 
+def plot_marker_gene_vplots(merged, cluster_ids, figure_name):
+    fig = plt.figure()
+    ax1 = plt.subplot(241)
+    ax2 = plt.subplot(242)
+    ax3 = plt.subplot(243)
+    ax4 = plt.subplot(244)
+    ax5 = plt.subplot(245)
+    ax6 = plt.subplot(246)
+    ax7 = plt.subplot(247)
+    ax8 = plt.subplot(248)
+    ax= [ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8]
+    cmap= sns.color_palette('husl',9)
 
-#' This plots the distribution of each marker gene as a violinplot in a subplot of the main figure  
-fig = plt.figure()
-ax1 = plt.subplot(241)
-ax2 = plt.subplot(242)
-ax3 = plt.subplot(243)
-ax4 = plt.subplot(244)
-ax5 = plt.subplot(245)
-ax6 = plt.subplot(246)
-ax7 = plt.subplot(247)
-ax8 = plt.subplot(248)
-ax= [ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8]
-cmap= sns.color_palette('husl',9)
+    #' V1 is the cluster ID column (in this example there are 10 clusters)
+    for i in range(len(merged.columns)-1):
+        sns.violinplot(data=[merged[merged.V1==cluster_ids[0]][merged.columns[i]],merged[merged.V1==cluster_ids[1]][merged.columns[i]],\
+                          merged[merged.V1==cluster_ids[2]][merged.columns[i]],merged[merged.V1==cluster_ids[3]][merged.columns[i]], \
+                          merged[merged.V1==cluster_ids[4]][merged.columns[i]], merged[merged.V1==cluster_ids[5]][merged.columns[i]],\
+                          merged[merged.V1==cluster_ids[6]][merged.columns[i]], merged[merged.V1==cluster_ids[7]][merged.columns[i]],\
+                          merged[merged.V1==cluster_ids[8]][merged.columns[i]], merged[merged.V1==cluster_ids[9]][merged.columns[i]]],\
+                       orient='h', cut=0,ax=ax[i], bw=2, scale='area', palette=cmap)
+        ax[i].set_xlim(0,10)
+        ax[i].set_title(merged.columns[i])
+    #'don't want y labels for all the plots (since y labels are cluster IDs) 
+        for i in [1,2,3,5,6,7]:
+            plt.setp(ax[i].get_yticklabels(), visible=False)
+    sns.despine()
+    fig.savefig(figure_name +'.pdf')
+  
+#' Usage for 8 genes and a dataset with 10 clusters 
+plot_marker_gene_vplots(merged_data(argv[1], argv[2]),range(10), argv[3])
 
-for i in range(len(merged.columns)-1):
-    sns.violinplot(data=[merged[merged.V1==0][merged.columns[i]],merged[merged.V1==1][merged.columns[i]],\
-                         merged[merged.V1==2][merged.columns[i]],merged[merged.V1==3][merged.columns[i]], \
-                         merged[merged.V1==4][merged.columns[i]], merged[merged.V1==5][merged.columns[i]],\
-                         merged[merged.V1==6][merged.columns[i]], merged[merged.V1==7][merged.columns[i]],\
-                         merged[merged.V1==8][merged.columns[i]], merged[merged.V1==9][merged.columns[i]]],\
-                   orient='h', cut=0,ax=ax[i], bw=2, scale='area', palette=cmap)
-    ax[i].set_xlim(0,10)
-    ax[i].set_title(merged.columns[i])
-    for i in [1,2,3,5,6,7]:
-        plt.setp(ax[i].get_yticklabels(), visible=False)
-
-sns.despine()
-
-# This saves the figure as a pdf file 
-fig.savefig(argv[3]+'.pdf')
